079b758e3bc88b8d3256c3bd00996412b808593a
LucasSuT <l1438s1438@gmail.com>
2021-10-21 14:15:58 +0800

    66. Plus One(Easy)

---
+++ Plus One/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size() - 1]++;
+        for (int i = digits.size() - 1; i >= 1; --i)
+        {
+            if (digits[i] > 9)
+            {
+                digits[i] -= 10;
+                digits[i - 1] += 1;
+            }
+            else return digits;
+        }
+        if (digits[0] > 9)
+        {
+            digits[0] -= 10;
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> tmp{ {1,1,1},{1,0,1},{1,1,1} };
+    vector<int> tmp2{ 2,0,2,1,1,0 };
+    Solution s;
+    //cout<<s.minWindow("aa", "aa");
+    //s.twoSum(tmp2,4);
+}
