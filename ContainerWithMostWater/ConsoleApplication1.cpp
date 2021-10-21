384d5a466c8b298fe4349341bb10d2705fa11f71
LucasSuT <l1438s1438@gmail.com>
2021-10-21 13:21:59 +0800

    11. Container With Most Water(Medium)

---
+++ ContainerWithMostWater/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0, j = height.size() - 1;
+        int max = 0, volume;
+        while (i != j)
+        {
+            volume = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
+            height[i] < height[j] ? i++ : j--;
+            max = max > volume ? max : volume;
+        }
+        return max;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> tmp{ {1,1,1},{1,0,1},{1,1,1} };
+    vector<int> tmp2{ 2,0,2,1,1,0 };
+    Solution s;
+    //cout<<s.minWindow("aa", "aa");
+    //s.twoSum(tmp2,4);
+}
