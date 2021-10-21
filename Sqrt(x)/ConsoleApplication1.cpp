3985830151f2de08cb04269ca11c51d228c8e062
LucasSuT <l1438s1438@gmail.com>
2021-10-21 14:16:49 +0800

    69. Sqrt(x)(Easy)

---
+++ Sqrt(x)/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)return 0;
+        int i = 1;
+        for (i; i <= x; ++i)
+        {
+            if ((long)i * i > x)
+            {
+                return --i;
+            }
+            else if (i * i == x)return i;
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> tmp{ {1,1,1},{1,0,1},{1,1,1} };
+    vector<int> tmp2{ 2,0,2,1,1,0 };
+    Solution s;
+    //cout<<s.minWindow("aa", "aa");
+    //s.twoSum(tmp2,4);
+}
