8db47cd76443cb258df7bc85122389d4a86f42a2
LucasSuT <l1438s1438@gmail.com>
2021-10-21 13:50:24 +0800

    48. Rotate Image(Medium)

---
+++ Rotate Image/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[i].size() / 2; j++)
+            {
+                swap(matrix[i][j], matrix[i][matrix[i].size() - j - 1]);
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[i].size() - i; j++)
+            {
+                swap(matrix[i][j], matrix[matrix.size() - j - 1][matrix.size() - i - 1]);
+            }
+        }
+    }
+};
+
+int main()
+{
+    vector<vector<int>> tmp{ {1,1,1},{1,0,1},{1,1,1} };
+    vector<int> tmp2{ 2,0,2,1,1,0 };
+    Solution s;
+    //cout<<s.minWindow("aa", "aa");
+    //s.twoSum(tmp2,4);
+}
