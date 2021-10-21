64083c2313ef4715d8b24a765eb6beb6ae02366b
LucasSuT <l1438s1438@gmail.com>
2021-10-21 13:32:22 +0800

    21. Merge Two Sorted Lists(Easy)

---
+++ Merge Two Sorted Lists/ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode();
+        ListNode* temp = result;
+        while (l1 != nullptr && l2 != nullptr)
+        {
+            if (l2->val < l1->val)
+            {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            else
+            {
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            temp = temp->next;
+        }
+        if (l1)
+        {
+            temp->next = l1;
+        }
+        else if (l2)
+        {
+            temp->next = l2;
+        }
+        return result->next;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> tmp{ {1,1,1},{1,0,1},{1,1,1} };
+    vector<int> tmp2{ 2,0,2,1,1,0 };
+    Solution s;
+    //cout<<s.minWindow("aa", "aa");
+    //s.twoSum(tmp2,4);
+}
