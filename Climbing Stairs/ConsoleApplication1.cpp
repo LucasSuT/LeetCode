5ee64a31fd0f9324199717098dde4be65d7bc048
LucasSuT <l1438s1438@gmail.com>
2021-10-21 14:17:35 +0800

    70. Climbing Stairs(Easy)

---
+++ Climbing Stairs/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n < 3)return n;
+        int* table = new int[n + 1]{ 0,1,2 };
+        for (int i = 3; i <= n; i++)
+        {
+            table[i] = table[i - 1] + table[i - 2];
+        }
+        return table[n];
+    }
+};
+
+int main()
+{
+    vector<vector<int>> tmp{ {1,1,1},{1,0,1},{1,1,1} };
+    vector<int> tmp2{ 2,0,2,1,1,0 };
+    Solution s;
+    //cout<<s.minWindow("aa", "aa");
+    //s.twoSum(tmp2,4);
+}
