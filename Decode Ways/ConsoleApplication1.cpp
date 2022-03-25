96941c9ca98b0c81b953efca4d32599dbed293c6
LucasSuT <l1438s1438@gmail.com>
2022-03-25 16:23:47 +0800

    91. Decode Ways

---
+++ Decode Ways/ConsoleApplication1.cpp
@@ -0,0 +1,73 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* pre;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        pre = NULL;
+    }
+};
+
+class Solution {
+public:
+    unordered_map<string, int>umap;
+    int numDecodings(string s) {
+        if (umap[s])return umap[s];
+        else if (s.length() == 0)return 1;
+        else if (s[0] == '0')return 0;
+
+        if (s.length() >= 2 && stoi(s.substr(0,2))<27)
+            umap[s] += numDecodings(s.substr(1)) + numDecodings(s.substr(2));
+        else umap[s] += numDecodings(s.substr(1));
+        return umap[s];
+    }
+};
+
+int main()
+{
+    //vector<vector<int>> tmp{ {1, 2}, { 1,3 },{1,8},{2,6},{3,1},{3,10} };
+    vector<int> tmp2{ 2,2,1 };
+    vector<int> tmp3{ 4,4,1,5,1 };
+    //vector<vector<char>> tmp3{ {'A', 'A', 'A', 'A','A','A'}, {'A', 'A', 'A', 'A','A','A'}};/*,{'A', 'A', 'A', 'A','A','A'},{'A', 'A', 'A', 'A','A','A'}
+    //                            ,{'A', 'A', 'A', 'A','A','A'},{'A', 'A', 'A', 'A','A','A'} };*/
+    Solution s;
+    cout << s.numDecodings("11111");
+}
