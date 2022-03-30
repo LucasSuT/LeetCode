a63da2e18a4d90b1899ddbb0bcd244f6b8c36cc6
LucasSuT <l1438s1438@gmail.com>
2022-03-30 13:45:57 +0800

    160. Intersection of Two Linked Lists

---
+++ Intersection of Two Linked Lists/ConsoleApplication1.cpp
@@ -0,0 +1,97 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+#include <set>
+
+using namespace std;
+
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x, ListNode* n) : val(x), next(n) {}
+};
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* pre;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        pre = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        int length_A = 1, length_B = 1;
+
+        if (LastNode(headA, length_A) != LastNode(headB, length_B))return NULL;
+        cout << length_A << " " << length_B << "\n";
+        if (length_A > length_B)
+            for (int i = 0; i < length_A - length_B; i++)headA = headA->next;
+        else if(length_A < length_B)
+            for (int i = 0; i < length_B - length_A; i++)headB = headB->next;
+
+        return FindInsection(headA, headB);
+    }
+
+    ListNode* LastNode(ListNode* head, int& length)
+    {
+        while (head->next != NULL)
+        {
+            head = head->next;
+            length++;
+        }
+        return head;
+    }
+
+    ListNode* FindInsection(ListNode* headA, ListNode* headB)
+    {
+        while (headA != headB)
+        {
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return headA;
+    }
+};
+
+int main()
+{
+    //vector<vector<int>> tmp{ {1, 2}, { 1,3 },{1,8},{2,6},{3,1},{3,10} };
+    vector<int> tmp2{ 2,3,-2,4 };
+    vector<int> tmp3{ 4,4,1,5,1 };
+    //vector<vector<char>> tmp3{ {'A', 'A', 'A', 'A','A','A'}, {'A', 'A', 'A', 'A','A','A'}};/*,{'A', 'A', 'A', 'A','A','A'},{'A', 'A', 'A', 'A','A','A'}
+    //                            ,{'A', 'A', 'A', 'A','A','A'},{'A', 'A', 'A', 'A','A','A'} };*/
+    /*Solution s;
+    cout << s.maxProduct(tmp2);*/
+}
