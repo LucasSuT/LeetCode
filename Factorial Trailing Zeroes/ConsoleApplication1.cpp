bb37f1504b87313fceef7bf15f1c580a7383413d
LucasSuT <l1438s1438@gmail.com>
2022-04-12 10:19:56 +0800

    172. Factorial Trailing Zeroes(Medium)

---
+++ Factorial Trailing Zeroes/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0;
+        int i = 1;
+        while (n / 5)
+        {
+            n -= 5;
+            sum++;
+        }
+        
+        return sum;
+    }
+};
+
+
+int main()
+{
+   
+    system("pause");
+}
