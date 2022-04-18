e090b7af950b1f55014e09097e40476dcb53385a
LucasSuT <l1438s1438@gmail.com>
2022-04-18 14:52:46 +0800

    190. Reverse Bits(Easy)

---
+++ Reverse Bits/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int shift = -1;
+        uint32_t result = 0x00;
+        while (++shift < 32)
+            result = result << 1 | n >> shift & 0x01;
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> vec{ 34323,3432 };
+    cout<<s.reverseBits(156);
+    system("pause");
+}
