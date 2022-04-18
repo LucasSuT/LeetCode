cd4689fbc1cb85a8425ea90982c84f318a91dd66
LucasSuT <l1438s1438@gmail.com>
2022-04-18 14:59:32 +0800

    191. Number of 1 Bits(Easy)

---
+++ Number of 1 Bits/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include "Windows.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int result = 0;
+        int shift = 0;
+        while (++shift < 32)
+            result += n >> shift & 0x01;
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> vec{ 34323,3432 };
+    cout<<s.reverseBits(156);
+    system("pause");
+}
