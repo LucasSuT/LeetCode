01980b9f2220d7006e934a41b1abfdb067d19210
LucasSuT <l1438s1438@gmail.com>
2022-08-16 11:21:29 +0800

    1.Two Sum(Easy) 60%

---
+++ Language C/Two Sum/LeetCodeCVS.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+	int* result = (int*)malloc(sizeof(int) * 2);
+	*returnSize = 2;
+	for (int i = 0; i < numsSize - 1; i++)
+	{
+		for (int j = i + 1; j < numsSize; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				result[0] = i;
+				result[1] = j;
+				return result;
+			}
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	int data[] = { 2,7,11,15 };
+	int return_size = 2;
+	int *result = twoSum(data, 4, 22, &return_size);
+	printf("%d %d\n", result[0], result[1]);
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
