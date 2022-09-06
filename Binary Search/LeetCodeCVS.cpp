b58c56468cfe954b32ea98d88eb16e2a1613b5ba
LucasSuT <l1438s1438@gmail.com>
2022-09-06 16:42:02 +0800

    704. Binary Search(EASY) 100%

---
+++ Binary Search/LeetCodeCVS.cpp
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+//Definition for singly-linked list.
+struct List {
+	struct TreeNode* tree_node;
+	struct List* next;
+};
+struct List* tail;
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+int compare(const void* a, const void* b)
+{
+	return *(char*)a - *(int*)b;
+}
+
+int search(int* nums, int numsSize, int target) {
+	int i = 0, j = numsSize - 1, mid;
+	while (i < j)
+	{
+		mid = j + i / 2;
+		if (nums[mid] < target)
+		{
+			i = mid + 1;
+		}
+		else if (nums[mid > target])
+		{
+			j = mid - 1;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	char a[] = "nagaram";
+	char b[] = "anagram";
+
+	isAnagram(a, b);
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
