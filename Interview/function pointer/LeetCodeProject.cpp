2d523130fcbfc38b83624cd03973d1088d1aa98e
LucasSuT <l1438s1438@gmail.com>
2022-11-08 13:18:00 +0800

    function pointer example

---
+++ Interview/function pointer/LeetCodeProject.cpp
@@ -0,0 +1,35 @@
+﻿// LeetCodeProject.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Sort(int* nums1, int numsSize) {
+}
+
+void test1() { printf("test1\n"); }
+void test2() { printf("test2\n"); }
+void test3() { printf("test3\n"); }
+
+void (*func[3])() = {test1,test2,test3};
+
+void test(void(*func)()) {
+	func();
+}
+
+int main()
+{
+	func[1]();
+	test(func[0]);
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
